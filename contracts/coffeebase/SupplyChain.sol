7ab49e8372eb4366b3a0a6876549130664745ee0
skybluedragon <tienhanprab@gmail.com>
2021-03-11 22:00:39 +0800

    import

--- contracts/coffeebase/SupplyChain.sol
+++ contracts/coffeebase/SupplyChain.sol
@@ -6,7 +6,7 @@ import "../coffeeaccesscontrol/FarmerRole.sol";
 import "../coffeeaccesscontrol/RetailerRole.sol";
 
 // Define a contract 'Supplychain'
-contract SupplyChain {
+contract SupplyChain is ConsumerRole, DistributorRole, FarmerRole, RetailerRole{
 
   // Define 'owner'
   address owner;
@@ -159,7 +159,16 @@ contract SupplyChain {
   }
 
   // Define a function 'harvestItem' that allows a farmer to mark an item 'Harvested'
-  function harvestItem(uint _upc, address _originFarmerID, string _originFarmName, string _originFarmInformation, string  _originFarmLatitude, string  _originFarmLongitude, string  _productNotes) public 
+  function harvestItem(
+    uint _upc, 
+    address _originFarmerID, 
+    string _originFarmName, 
+    string _originFarmInformation, 
+    string  _originFarmLatitude, 
+    string  _originFarmLongitude, 
+    string  _productNotes) 
+    public
+    only
   {
     // Add the new item as part of Harvest
     
